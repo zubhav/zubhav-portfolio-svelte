54841b4fdc7324aea301d66947791f03eb0ca17f
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-17 22:53:15 +0000

    Remove build env

--- api/icons.ts
+++ api/icons.ts
@@ -1,6 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node'
 const contentful = require('contentful')
 
+console.log(process.env.CONTENTFUL_ACCESS_TOKEN)
+
 export default (request: NowRequest, response: NowResponse) => {
   const client = contentful.createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
