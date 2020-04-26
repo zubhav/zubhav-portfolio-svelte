03b144fa3f665b0da8acdb15bb841122d7336de9
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-04-26 23:44:36 +0100

    function async

--- api/icons.ts
+++ api/icons.ts
@@ -1,31 +1,35 @@
-import { NowRequest, NowResponse } from '@now/node'
-const contentful = require('contentful')
+import { NowRequest, NowResponse } from "@now/node";
+const contentful = require("contentful");
 
-export default (request: NowRequest, response: NowResponse) => {
+export default async (request: NowRequest, response: NowResponse) => {
   const client = contentful.createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   });
-  
-  let items = []
 
-  client.getEntries({ content_type: 'landingIcon', order: 'fields.order' })
-    .then(entries => {
-      items = entries.items.map(icon => {
-        const { sys, fields } = icon
-        const { title, file, link: href } = fields
-        const id = sys.id
-        const iconHref = file.fields.file.url
+  try {
+    const entries = client.getEntries({
+      content_type: "landingIcon",
+      order: "fields.order",
+    });
 
-        return {
-          id,
-          title,
-          href,
-          iconHref
-        }
-      })
+    const items = entries.items.map((icon) => {
+      const { sys, fields } = icon;
+      const { title, file, link: href } = fields;
+      const id = sys.id;
+      const iconHref = file.fields.file.url;
 
-      response.status(200).json(items)
-    })
-    .catch(console.error)
-}
+      return {
+        id,
+        title,
+        href,
+        iconHref,
+      };
+    });
+
+    response.status(200).json(items);
+  } catch (err) {
+    console.error(err);
+    response.status(500);
+  }
+};
