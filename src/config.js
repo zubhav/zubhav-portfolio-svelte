39458c906d27f6961dcc81b2135e57e7dee10901
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-05-29 00:55:15 +0100

    absolute

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-  DEV_API_URL: "https://zubhav.dev",
+  DEV_API_URL: "http://localhost.sub:3000",
+  PROD_API_URL: "https://zubhav.dev",
 };
 
 export default CONFIG;
--- src/utils/fetchFromApi.utils.js
+++ src/utils/fetchFromApi.utils.js
@@ -2,7 +2,9 @@ import fetch from "isomorphic-fetch";
 import CONFIG from "../config";
 
 const baseUrl =
-  process.env.NODE_ENV === "development" ? CONFIG.DEV_API_URL : "";
+  process.env.NODE_ENV === "development"
+    ? CONFIG.DEV_API_URL
+    : CONFIG.PROD_API_URL;
 
 export const fetchFromApi = (path, options = {}) => {
   return fetch(`${baseUrl}${path}`, options);
