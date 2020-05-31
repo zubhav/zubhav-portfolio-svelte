e951dba124c783772915d07aba4deb4d388500bd
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-05-31 23:03:04 +0100

    blm

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,14 +1,10 @@
 <script>
-	const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 </script>
 
 <footer class="bottom-0 left-0 h-12 fixed w-full">
-    <section class="border-t border-white border-solid py-2 mx-5">
-        <p class="float-left">
-            &copy; Copyright {year}
-        </p>
-        <p class="float-right">
-            Zubhav Ltd
-        </p>
-    </section>
+  <section class="border-t border-white border-solid py-2 mx-5">
+    <p class="float-left">&copy; Copyright {year}</p>
+    <p class="float-right">Zubhav Ltd</p>
+  </section>
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,79 +1,45 @@
-<script context="module">
-  import { fetchFromApi } from "../utils/fetchFromApi.utils";
-
-  export async function preload(page, session) {
-    const res = await fetchFromApi("/api/icons");
-    const items = await res.json();
-    return { items };
-  }
-</script>
-
 <script>
-  export let items;
-
   import { fade } from "svelte/transition";
 
   let visible = false;
+  let quoteVisible = true;
+
+  let quotes = [
+    "When 'I' replaced with 'We', even the illness becomes wellness. - Malcolm X",
+    "There comes a time when silence is betrayal. - Martin Luther King, Jr.",
+    "Ideologies separate us. Dreams and anguish bring us together. – Eugene Ionesco"
+  ];
+
+  let selectedQuoteIdx = 0;
+  let selectedQuote = quotes[0];
+
+  $: {
+    selectedQuote = quotes[selectedQuoteIdx % quotes.length];
+  }
 
   setTimeout(() => {
     visible = true;
   }, 200);
+
+  setInterval(() => {
+    quoteVisible = false;
+    selectedQuoteIdx += 1;
+
+    setTimeout(() => {
+      quoteVisible = true;
+    }, 1000);
+  }, 6500);
 </script>
 
 <style>
-  .avatar {
-    box-shadow: 10px 7px 30px rgba(0, 0, 0, 0.35);
-  }
 
-  .landing-icon {
-    box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.25);
-  }
-
-  .linkedin img {
-    margin-left: 2px;
-    margin-bottom: 2px;
-  }
-
-  .twitter img {
-    margin-top: 3px;
-    margin-left: 2px;
-  }
-
-  .github img {
-    margin-top: 3px;
-    margin-right: 1px;
-  }
-
-  .stackoverflow img {
-    margin-left: 2px;
-    margin-bottom: 3px;
-  }
-
-  @media (max-width: 700px) {
-    .cv {
-      width: 40%;
-    }
-  }
 </style>
 
 <svelte:head>
-  <title>Subhav Gautam</title>
+  <title>Black Lives Matter</title>
 </svelte:head>
 
-<section class="text-center flex h-screen">
-  {#if visible}
-    <a
-      transition:fade
-      href="mailto:me@zubhav.dev?subject=Request for CV&body=Hi Subhav, I'm
-      ____.%0D%0DCould you please send me a copy of your CV?"
-      title="Request my CV">
-      <img
-        class="cv absolute top-0 left-0 w-1/4"
-        alt="Request my CV"
-        src="./images/cv.png" />
-    </a>
-  {/if}
-
+<section class="text-center flex h-screen w-full">
   {#if visible}
     <section
       class="flex flex-col mx-auto justify-center xs:mb-16"
@@ -81,31 +47,21 @@
       <aside class="pb-2">
         <img
           class="avatar mx-auto rounded-full"
-          alt="Subhav Gautam avatar"
-          src="./images/avatar.png"
-          width="292" />
+          alt="Black Lives Matter"
+          src="./images/blm.png"
+          width="40%" />
       </aside>
 
       <header class="font-bold">
-        <h1 class="text-4xl leading-relaxed">Subhav Gautam</h1>
-        <h2 class="text-lg pt-2">
-          Software Engineer specialising in web technologies
-        </h2>
+        <h1 class="text-4xl leading-relaxed">Black Lives Matter</h1>
+        <section style="min-height: 100px;">
+          {#if quoteVisible}
+            <h2 class="text-lg pt-2" transition:fade>
+              {selectedQuote ? selectedQuote : ''}
+            </h2>
+          {/if}
+        </section>
       </header>
-
-      <ul class="flex items-center justify-center h-16 mt-6">
-        {#each items as item (item.id)}
-          <li class="mr-6">
-            <a
-              class={`landing-icon bg-white rounded-full flex justify-center items-center h-16 w-16 ${item.title.toLowerCase()}`}
-              href={item.href}
-              target="_blank"
-              rel="noopener noreferrer">
-              <img alt={item.title} src={item.iconHref} />
-            </a>
-          </li>
-        {/each}
-      </ul>
     </section>
   {/if}
 </section>
